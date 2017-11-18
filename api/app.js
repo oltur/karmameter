8f69058e4d1181e64713a38c94813a3c7e300275
oltur <me@turevski.com>
2017-11-18 19:21:12 +0100

    initial commit

---
+++ api/app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) { return res.send('Hello World!') })
+
+app.listen(3000, function () { console.log('Example app listening on port 3000!') } )
