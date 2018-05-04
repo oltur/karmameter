3bb0053833be06abacde19b5297639ae0ff68573
Alex Turevski <me@turevski.com>
2018-05-04 23:01:38 +0200

    Docs

--- 1/scripts/maps.js
+++ 1/scripts/maps.js
@@ -1,35 +1,34 @@
 export default class Maps {
+  Map() {
+    this.map;
+    this.service;
+    this.infowindow;
+  }
 
-    Map() {
-        this.map;
-        this.service;
-        this.infowindow
+  initialize() {
+    let pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
+
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15,
+    });
+
+    const request = {
+      location: pyrmont,
+      radius: '500',
+      type: ['restaurant'],
+    };
+
+    this.service = new google.maps.places.PlacesService(this.map);
+    this.service.nearbySearch(request, this.callback);
+  }
+
+  callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (let i = 0; i < results.length; i++) {
+        let place = results[i];
+        createMarker(results[i]);
+      }
     }
-
-    initialize() {
-        var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
-
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: pyrmont,
-            zoom: 15
-        });
-
-        var request = {
-            location: pyrmont,
-            radius: '500',
-            type: ['restaurant']
-        };
-
-        service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
-    }
-
-    callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var place = results[i];
-                createMarker(results[i]);
-            }
-        }
-    }
+  }
 }
